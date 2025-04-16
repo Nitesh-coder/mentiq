9f87216c9ae5e615c86357f29fc583664b45c039
Nitesh Ghimire <initesh100@gmail.com>
2025-04-16 12:41:40 +0545

    add some upcoming message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ export default function Home (){
   return(
     <div>
       <Nav />
+      <div>
+        <h1 className=" text-4xl">Something is cooking stay tune...</h1>
+        <p>For contact:</p>
+        <p>email: niteshghimire223@gmail.com</p>
+        <p>whatsapp: +9779811393769</p>
+      </div>
     </div>
   )
 }
